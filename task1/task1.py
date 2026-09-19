94bce1080d3fd6732664577bc26de80eacfd4ea7
Tabyreto4ka1 <sashakorostij@gmail.com>
2026-09-19 19:02:18 +0300

    first commit

--- task1/task1.py
+++ task1/task1.py
@@ -2,6 +2,7 @@ import shutil
 from pathlib import Path
 import logging
 from threading import Thread
+import sys
 
 class Threading():                              #Класс потоку
     def __init__(self,directory,dist):
@@ -10,13 +11,14 @@ class Threading():                              #Класс потоку
     def __call__(self):
         analis(self.directory, self.dist)
 
-def move(file:Path, folder_in_dist:Path):            #Функція, яка переміщає файл у потрібну папку
+def copying(file:Path, folder_in_dist:Path):            #Функція, яка переміщає файл у потрібну папку
     try: 
-        shutil.move(str(file), folder_in_dist)
+        shutil.copy(str(file), folder_in_dist)
     except FileNotFoundError :
         logging.debug("Директорію куди потрібно перенести файли не знайдено")
 
-def analis(directory, dist):                           #Функція яка аналізує папки і файли
+def analis(directory, dist):
+    print(sys.argv)                           #Функція яка аналізує папки і файли
     for file in directory.iterdir():
         if file.is_dir():                               #Якщо це папка - створюємо потік який при старті починає для себе функцію analis
             t=Threading(file, dist)
@@ -27,12 +29,19 @@ def analis(directory, dist):                           #Функція яка а
             sufix=str(file.suffix).replace(".","")
             new_folder=dist/sufix
             new_folder.mkdir(exist_ok=True)             #Якщо папки немає для вказаних розширень - створюємо
-            move(file, dist/sufix)
-   
+            copying(file, dist/sufix)
+
+
+
+
 if __name__=="__main__":
     logging.basicConfig(level=logging.DEBUG)
-    directory=Path(input("введіть шлях звідки переносити файли "))
-    dist=Path(input("Введіть шлях куди переносити файли "))
+    directory=Path(sys.argv[1])
+    if len(sys.argv)>2:
+        dist=Path(sys.argv[2])
+    else:    
+        dist=Path("dist")
+        dist.mkdir()
     if directory.is_dir():          
         analis(directory, dist)     
     else:
